26e4915f293da362df9e9263d93adfba922d799b
Peshel <gomopeshelo@gmail.com>
2025-06-18 00:07:22 +0200

    b4 presentation

--- src/app/lib/connection.js
+++ src/app/lib/connection.js
@@ -2,8 +2,8 @@ import PocketBase from 'pocketbase';
 
 // const pb = new PocketBase('http://127.0.0.1:8090');
 
-// const pb = new PocketBase('https://swiftly.pockethost.io/');
-const pb = new PocketBase('http://4.222.232.224:8080/');
+const pb = new PocketBase('https://swiftly.pockethost.io/');
+// const pb = new PocketBase('http://4.222.232.224:8080/');
 
 
 export default pb;
